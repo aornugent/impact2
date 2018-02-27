fbb125a578a27311bd58c8322888a7cd2612bfb9
unknown <s433460@5Y19R42.ucstaff.win.canberra.edu.au>
2018-02-27 11:02:32 +1100

    Update documentation

--- R/utils.R
+++ R/utils.R
@@ -1,15 +1,3 @@
-#' Pipe operator
-#'
-#' See \code{magrittr::\link[magrittr]{\%>\%}} for details.
-#'
-#' @name %>%
-#' @rdname pipe
-#' @keywords internal
-#' @export
-#' @importFrom magrittr %>%
-#' @usage lhs \%>\% rhs
-NULL
-
 #' Function to write to console
 #'
 #' @name printf
@@ -18,3 +6,50 @@ NULL
 #' @usage printf("Hello world")
 
 printf <- function(...) cat(sprintf(...), "\n")
+
+#' Load dplyr
+#'
+#' dplyr is used extensively in this package
+#'
+#' @name dplyr
+#' @import dplyr
+NULL
+
+#' Basic ggplot theme
+#'
+#' Set as default on package load.
+#'
+#' @import ggplot2
+
+.onLoad <- function(libname, pkgname) {
+
+  theme_impact <-
+    theme_minimal() +
+      theme(
+        panel.grid = element_blank(),
+        #panel.border = element_rect(fill = NA),
+        axis.line.x = element_line(
+          color="black",
+          size = 0.5),
+        axis.line.y = element_line(
+          color="black",
+          size = 0.5),
+        axis.ticks = element_line(),
+        panel.spacing = unit(1, "lines"),
+        plot.title = element_text(
+          margin = margin(t = 10, b = 20),
+          hjust = 0),
+        legend.position = "bottom",
+        aspect.ratio = 1)
+
+  theme_set(theme_impact)
+}
+
+#' expand.grid for dataframes
+#'
+#' @param ... a dataframe
+#' @usage df_long <- expand.grid.df(df)
+
+expand_grid <- function(...) {
+  Reduce(function(...) merge(..., by=NULL), list(...))
+}
